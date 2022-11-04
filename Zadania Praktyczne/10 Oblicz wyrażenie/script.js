76b9a329455d2adba6b59b2c1e1b6e35fe445a9b
Domin122 <dgabrysz3@gmail.com>
2022-11-04 10:31:14 +0100

    commit

---
+++ Zadania Praktyczne/10 Oblicz wyrażenie/script.js
@@ -0,0 +1,13 @@
+const liczba_x = document.querySelector('#liczba_a');
+const wynik = document.querySelector('#wynik');
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', function() {
+    let x =parseInt(liczba_x.value);
+    let wyrazenie = x^2/(1+x)^2;
+    wynik_tekst +=
+
+    let wynik_tekst = ``;
+
+    wynik.innerHTML = wynik_tekst;
+});
