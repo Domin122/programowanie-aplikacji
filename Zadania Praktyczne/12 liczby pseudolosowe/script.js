2c3522cfbc372b07135dda57edc3f5ae680bce12
Domin122 <dgabrysz3@gmail.com>
2022-11-17 08:08:57 +0100

    commit

--- Zadania Praktyczne/12 liczby pseudolosowe/script.js
+++ Zadania Praktyczne/12 liczby pseudolosowe/script.js
@@ -1,16 +1,16 @@
-const min= document.querySelector('#min');
+const min = document.querySelector('#min');
 const max = document.querySelector('#max');
 const wynik = document.querySelector('#wynik');
 const btn = document.querySelector('button');
 
 btn.addEventListener('click', function() {
-    let min =parseInt(min.value);
-    let max =parseInt(max.value);
-    let w1 = Math.floor(Math.random()*(max-min+1)+min);
-    let w2 = Math.floor(Math.random()*(max-min+1)+min);
-    let w3 = Math.floor(Math.random()*(max-min+1)+min);
-    let w4 = Math.floor(Math.random()*(max-min+1)+min);
-    let w5 = Math.floor(Math.random()*(max-min+1)+min);
+    let m =parseInt(min.value);
+    let x =parseInt(max.value);
+    let w1 = Math.floor(Math.random()*(x-m+1)+m);
+    let w2 = Math.floor(Math.random()*(x-m+1)+m);
+    let w3 = Math.floor(Math.random()*(x-m+1)+m);
+    let w4 = Math.floor(Math.random()*(x-m+1)+m);
+    let w5 = Math.floor(Math.random()*(x-m+1)+m);
     let suma = w1 + w2 + w3 + w4 + w5;
     let iloczyn = w1 * w2 * w3 * w4 * w5 ;
     let srednia = w1 + w2 + w3 + w4 + w5/5;
