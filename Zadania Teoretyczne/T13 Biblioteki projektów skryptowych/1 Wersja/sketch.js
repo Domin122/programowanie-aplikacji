5b4f74ad9c75888c81b3476667d011ed4a0cde89
Domin122 <dgabrysz3@gmail.com>
2022-11-24 08:32:29 +0100

    commit

---
+++ Zadania Teoretyczne/T13 Biblioteki projektów skryptowych/1 Wersja/sketch.js
@@ -0,0 +1,39 @@
+function setup() {
+    createCanvas(600, 600);
+}
+function draw() {
+    background(255);
+    let k = color(0, 204, 255);
+    fill(k);
+    noStroke();
+    rect(120, 165, 260, 75);
+
+    let l = color(0, 204, 255);
+    fill(l);
+    noStroke();
+    rect(225, 122, 100, 75);
+
+    let g = color(128, 128, 128);
+    fill(g);
+    noStroke();
+    rect(280, 128, 30, 30);
+
+
+    let h = color(255, 204, 0);
+    fill(h);
+    noStroke();
+    ellipse(380, 180, 25, 25);
+
+    let c = color(0, 0, 0);
+    fill(c);
+    noStroke();
+    ellipse(345, 250, 55, 55);
+    ellipse(155, 250, 55, 55);
+    rect(468, 120, 19, 135)
+
+    let a = color(255, 0, 0);
+    fill(a);
+    noStroke();
+    rect(448, 90, 60, 60, 20);
+
+}
