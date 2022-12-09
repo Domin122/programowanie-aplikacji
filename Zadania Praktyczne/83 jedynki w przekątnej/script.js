85198beed5d7c6d94ad06c1e7777a150ca17ef06
Domin122 <dgabrysz3@gmail.com>
2022-12-09 10:31:14 +0100

    commit

--- Zadania Praktyczne/83 jedynki w przekątnej/script.js
+++ Zadania Praktyczne/83 jedynki w przekątnej/script.js
@@ -4,13 +4,11 @@ const wynik = document.querySelector(`#wynik`)
 
 btn.addEventListener(`click`, function () {
     wynik.innerHTML = ``
-   for(i=1; i<a.value ; i++){
-
-   }
-
-
-
-
-
+    for(i=1; i<=a.value ; i++){
+        for(j=0; j<i; j++){
+            wynik.innerHTML += `0`
+        }
+        wynik.innerHTML += `<br>`
+    }
 
 });
