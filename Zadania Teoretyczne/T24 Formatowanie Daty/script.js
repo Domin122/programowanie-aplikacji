3f526804bad61e1fe798c60ea804db667fb256d4
Domin122 <dgabrysz3@gmail.com>
2023-01-09 09:31:19 +0100

    commit

--- Zadania Teoretyczne/T24 Formatowanie Daty/script.js
+++ Zadania Teoretyczne/T24 Formatowanie Daty/script.js
@@ -1,3 +1,7 @@
+const wynik = document.querySelector('#wynik');
+const btn = document.querySelector('button');
+btn.addEventListener('click', function() {
+
 function addZero(value) {
     return String(value).padStart(2, 0);
 }
@@ -26,4 +30,6 @@ const now = new Date();
 // const formatted = `${addZero(now.getDate())}.${addZero(now.getMonth() + 1)}.${now.getFullYear()}`; // 01.03.2021
 const formatted = `${addZero(now.getDate())} ${convertMonth(now.getMonth() + 1)} ${now.getFullYear()}`; // 01.03.2021
 
-console.log(formatted);
+    wynik.innerHTML = `Dzisiejsza data: <br> ${formatted}`;
+
+});
